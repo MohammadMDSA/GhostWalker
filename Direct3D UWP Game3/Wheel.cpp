8439ec6bcf29c460d27be172ec10ceff18c706cd
Mohammad MDSA <samimd.77@gmail.com>
2018-03-16 15:51:54 +0330

    Added Wheel class

---
+++ Direct3D UWP Game3/Wheel.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "Wheel.h"
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+
+Wheel::Wheel()
+{
+
+	m_world *= DirectX::SimpleMath::Matrix::CreateScale(0.005);
+}
+
+void Wheel::Draw(ID3D11DeviceContext3 * deviceContext, DirectX::SimpleMath::Matrix view, DirectX::SimpleMath::Matrix proj)
+{
+	m_model->Draw(deviceContext, *m_states.get(), m_world, view, proj);
+}
+
+void Wheel::Update(DX::StepTimer const & timer)
+{
+	m_world *= Matrix::CreateRotationZ(0.01f);
+}
+
+void Wheel::CreateResource(DeviceResource^ device, DirectX::XMFLOAT3 & size, bool rhcoords, bool invertn)
+{
+
+	EffectFactory fx(device->GetDevice());
+	m_model = Model::CreateFromSDKMESH(device->GetDevice(), L"Senza Titolo 2.sdkmesh", fx);
+}
+
---
+++ Direct3D UWP Game3/Wheel.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include "GameObject.h"
+#include <Model.h>
+#include <CommonStates.h>
+#include <Effects.h>
+
+
+ref class Wheel : public GameObject
+{
+internal:
+	Wheel();
+
+	// Inherited via GameObject
+	virtual void Draw(ID3D11DeviceContext3 * deviceContext, DirectX::SimpleMath::Matrix view, DirectX::SimpleMath::Matrix proj) override;
+	virtual void Update(DX::StepTimer const & timer) override;
+	virtual void CreateResource(DeviceResource^ device, DirectX::XMFLOAT3 & size, bool rhcoords = true, bool invertn = false) override;
+
+private:
+
+	std::unique_ptr<DirectX::Model>			m_model;
+	std::unique_ptr<DirectX::CommonStates>	m_states;
+};
+

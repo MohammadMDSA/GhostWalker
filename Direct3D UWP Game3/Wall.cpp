85cdb97b29a66e4140b15d79687bcb6a9b2eb771
Mohammad MDSA <samimd.77@gmail.com>
2018-03-16 15:51:06 +0330

    Changed class name

---
+++ Direct3D UWP Game3/Wall.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "Wall.h"
+
+using namespace DirectX;
+
+
+static const XMVECTORF32 ROOM_BOUNDS = { 18.f, 16.f, 42.f, 0.f };
+
+
+Wall::Wall()
+{
+}
+
+
+void Wall::Draw(ID3D11DeviceContext3 * deviceContext, DirectX::SimpleMath::Matrix view, DirectX::SimpleMath::Matrix proj)
+{
+	m_shape->Draw(m_world, view, proj);
+}
+
+void Wall::Update(DX::StepTimer const & timer)
+{
+}
+
+void Wall::CreateResource(DeviceResource^ device, DirectX::XMFLOAT3 & size, bool rhcoords, bool invertn)
+{
+	GeometricPrimitive::CreateBox(device->GetDeviceContext(), XMFLOAT3(ROOM_BOUNDS[0], ROOM_BOUNDS[1], ROOM_BOUNDS[2]), false, true);
+}
--- Direct3D UWP Game3/Wall.h
+++ Direct3D UWP Game3/Wall.h
@@ -4,14 +4,14 @@
 
 #include <GeometricPrimitive.h>
 
-ref class Walls : public GameObject
+ref class Wall : public GameObject
 {
 internal:
-	Walls();
+	Wall();
 	// Inherited via GameObject
 	virtual void Draw(ID3D11DeviceContext3 * deviceContext, DirectX::SimpleMath::Matrix view, DirectX::SimpleMath::Matrix proj) override;
 	virtual void Update(DX::StepTimer const & timer) override;
-	virtual void CreateResource(ID3D11DeviceContext3 * deviceContext, DirectX::XMFLOAT3 & size, bool rhcoords = true, bool invertn = false) override;
+	virtual void CreateResource(DeviceResource^ device, DirectX::XMFLOAT3 & size, bool rhcoords = true, bool invertn = false) override;
 
 private:
 	std::unique_ptr<DirectX::GeometricPrimitive>			m_shape;
